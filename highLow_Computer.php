e3bd19968529a162790bbc35bf9975066f47cab5
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-20 10:44:52 -0500

    Added highLow game where the computer guesses a number

---
+++ highLow_Computer.php
@@ -0,0 +1,21 @@
+<?php
+echo "ENTER A NUMBER FOR THE COMPUTER TO GUESS BETWEEN 1 and 100: ";
+$magicNumber = fgets(STDIN);
+$min = 0;
+$max = 101;
+$count = 0;
+while ($input != $magicNumber) {
+  // fwrite(STDOUT,"MIN is $min and MAX is $max" . PHP_EOL);
+  $input = intval(($max + $min)/2);
+  echo "The Computer chose $input" . PHP_EOL;
+  if ($input < $magicNumber) {
+    echo "HIGHER" . PHP_EOL;
+    $min = $input;
+  }elseif ($input > $magicNumber) {
+    echo "LOWER" . PHP_EOL;
+    $max = $input;
+  }
+  $count += 1;
+  echo "The count is: $count" . PHP_EOL;
+}
+echo "GOOD JOB COMPUTER". PHP_EOL;
