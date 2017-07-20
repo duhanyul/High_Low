aed9b30be2a183e89c46a31360affbca9a4b7762
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-19 22:17:11 -0500

    Added high low game.

--- highLow.php
+++ highLow.php
@@ -1,1 +1,28 @@
-<?php 
+<?php
+$magicNumber = mt_rand(1,100);
+echo $magicNumber . PHP_EOL;
+fwrite(STDOUT,"Would you like to guess a number? (yes/no)" . PHP_EOL);
+$input = readline("");
+
+if ($input == 'yes') {
+  while ($input != 'EXIT') {
+    fwrite(STDOUT, "Guess a number" . PHP_EOL);
+    fwrite(STDOUT, "If you wish to quit type EXIT" . PHP_EOL);
+    $input = readline("");
+    if ($input < $magicNumber) {
+      fwrite(STDOUT, "HIGHER" . PHP_EOL);
+    }elseif ($input > $magicNumber) {
+      fwrite(STDOUT, "LOWER" . PHP_EOL);
+    }else {
+      fwrite(STDOUT, "Great Guess!" . PHP_EOL);
+      fwrite(STDOUT, "Play Again? (yes/no)" . PHP_EOL);
+      $input = readline("");
+      if ($input == "yes") {
+        $magicNumber = mt_rand(1,100);
+      }elseif ($input == 'no') {
+        $input = 'EXIT';
+        echo "Thanks for playing!";
+      }
+    }
+  }
+}
