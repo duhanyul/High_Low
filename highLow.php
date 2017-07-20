b33ac25c5f17e4cea15387677cefc857ea55b19b
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-20 09:24:11 -0500

    added ability to used passed argumetns to set number range.

--- highLow.php
+++ highLow.php
@@ -1,5 +1,15 @@
 <?php
-$magicNumber = mt_rand(1,100);
+echo $argv[1] . PHP_EOL;
+echo $argv[2] . PHP_EOL;
+if (is_numeric($argv[1]) && is_numeric($argv[2])) {
+  echo "Arguments Passed" . PHP_EOL;
+  if ($argv[1] < $argv[2]) {
+    $magicNumber = mt_rand($argv[1],$argv[2]);
+  }
+}else {
+  echo "No arguments passed" . PHP_EOL;
+  $magicNumber = mt_rand(1,100);
+}
 fwrite(STDOUT,"Would you like to guess a number? (yes/no)" . PHP_EOL);
 $input = readline("");
 
@@ -8,7 +18,10 @@ if ($input == 'yes') {
     fwrite(STDOUT, "Guess a number" . PHP_EOL);
     fwrite(STDOUT, "If you wish to quit type EXIT" . PHP_EOL);
     $input = readline("");
-    if ($input < $magicNumber) {
+    if ($input == "EXIT") {
+      echo "GOODBYE";
+    }
+    elseif ($input < $magicNumber) {
       fwrite(STDOUT, "HIGHER" . PHP_EOL);
     }elseif ($input > $magicNumber) {
       fwrite(STDOUT, "LOWER" . PHP_EOL);
